218e7071a53b3e0ec057fae1c24404a1a72900d9
cafaeinn <itwasnaow@gmail.com>
2025-05-08 23:03:16 +0800

    Update tiktok.js

--- tiktok.js
+++ tiktok.js
@@ -1,26 +1,28 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { color } = require('../../data/config.json');
 const { EmbedBuilder } = require('discord.js');
+const scraperModule = await import('@xct007/tiktok-scraper');
+const { default: TikTokScraper, someUtility } = scraperModule;
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('avatar')
-        .setDescription('returns your or the avatar of someone else')
-        .addUserOption(option =>
-            option.setName('user')
-                .setDescription('Tag a user to get their avatar'))
+        .setName('tiktok')
+        .setDescription('Download video dari tiktok')
+        .addStringOption(option =>
+            option.setName('url')
+                .setDescription('link tiktok'))
+	    	.setRequired(true)
 		.setDMPermission(false),
 
         async execute(interaction) {
-            let user = interaction.options.getUser('user');
-            if (user === null) {
-                user = interaction.user;
-            }
-            const avatar = user.displayAvatarURL({ size: 1024, extension: 'png', forceStatic: false });
-            const embed = new EmbedBuilder()
-                .setColor(color)
-                .setDescription(`You requested ${user}'s avatar`)
-                .setImage(avatar);
-            await interaction.reply({ embeds: [embed] });
+        	let url = interaction.options.getString('url');
+		let result = await TikTokScraper(url, { parse: true, keys: ['desc_language']});
+		//result.download.nowm
+		interaction.reply({
+			files: [{
+				attachment: result.download.nowm,
+				name: `${result.desc}.mp4`
+			}]
+		})
     },
 };
